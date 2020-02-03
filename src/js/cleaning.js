4e0320371a818262f54beea71c36cb067bd79767
MohamadAlGhorani <moh.gh266@gmail.com>
2020-02-03 14:49:53 +0100

    setup

---
+++ src/js/cleaning.js
@@ -0,0 +1,12 @@
+import { runApi } from "./runApi.js";
+
+const api =
+  "https://cors-anywhere.herokuapp.com/https://dbd-stats.info/api/characters";
+
+runApi(api).then(data => clean(data));
+
+function clean(data) {
+  console.log("DATA", data);
+}
+
+export { clean };
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+console.log("hello world!");
+import "babel-polyfill";
+
+import {
+    clean
+} from "./cleaning.js";
